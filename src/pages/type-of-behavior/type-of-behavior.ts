746282d4e918e48bdd1effdc593a3ff874277d9c
Mendiola_esteban@outlook.com <Mendiola_esteban@outlook.com>
2018-10-10 17:00:28 -0500

    Validacion Type Of Behavior

--- src/pages/type-of-behavior/type-of-behavior.ts
+++ src/pages/type-of-behavior/type-of-behavior.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the TypeOfBehaviorPage page.
@@ -18,11 +19,25 @@ import { HomePage } from '../home/home';
 export class TypeOfBehaviorPage {
   public kindSex=this.navParams.get("sex");
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   backPage() {
     this.navCtrl.push(HomePage);
+  }
+  nextPage(typeBehavior,cant){
+    if (typeBehavior == null || cant == null) {
+      let alert = this.alertCtrl.create({
+        title: 'Datos Incorrectos',
+        subTitle: 'Porfavor ingrese el tipo de comportamiento y el numero de ratas '+this.navParams.get("sex"),
+        buttons: ['OK']
+      });
+      alert.present();
+    }else{
+      console.log("next");
+      
+    }
+
   }
 
 }
